96b92d1b19a65a6704942b4e7fdcd876d0b51fd9
ThomasBorges <thomas.mr.borges@gmail.com>
2020-05-26 21:41:38 -0300

    add event timout for cursing

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 import os
+import json
 from twitchio.ext import commands
+from operator import itemgetter
+
+curse_words = {}
+with open('curse_words.json') as json_file:
+    curse_words = json.load(json_file)
+
+users = []
 
 # set up the bot
 bot = commands.Bot(
@@ -21,37 +29,85 @@ async def event_ready():
 @bot.event
 async def event_message(ctx):
     'Runs every time a message is sent in chat.'
-
-    # make sure the bot ignores itself and the streamer
     if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+        # make sure the bot ignores itself and the streamer
         return
 
-    await bot.handle_commands(ctx)
+    #content aimed to him
+    if 'bototario' in ctx.content.lower():
+        await bot.handle_commands(ctx)
+        
 
-    # await ctx.channel.send(ctx.content)
+        #timeout user if he curses bototario five times
+        for curse in curse_words['listen']:
+            if curse in ctx.content.lower():
+                if ctx.author.name in map(itemgetter('user'), users):
+                    index = list(map(itemgetter('user'), users)).index(ctx.author.name)
+                    users[index]['times_cursed'] += 1
 
-    if ctx.author.name == 'forthejacs':
-        await ctx.channel.send(f"que voz linda a sua")
 
+                    if users[index]['times_cursed'] <= 2: 
+                        await ctx.channel.send(f"mano, to sem paciência nem vem")
+                        return
+                    if users[index]['times_cursed'] == 3: 
+                        await ctx.channel.send(f"@{ctx.author.name} ce ta me irritano")
+                        return
+                    if users[index]['times_cursed'] == 4: 
+                        await ctx.channel.send(f"se chingar de novo vai toma TO")
+                        return
+                    if users[index]['times_cursed'] == 5: 
+                        await ctx.channel.send(f"tomaaa, agora fica quieto ai palhaço")
+                        users[index]['times_cursed'] = 0
+                        await ctx.channel.timeout(ctx.author.name, 60)
+                        return
+
+                    return
+
+                else:
+                    users.append({
+                        "user":ctx.author.name,
+                        "times_cursed":0
+                    })
+                    await ctx.channel.send(f"oxi, te dei liberdade de falar coisas chulas comigo é?")
+                    return
+
+
+        content = ctx.content.lower()
+        #greetings
+        if 'olá' in content or 'eae' in content or 'oi' in content or 'fala' in content or 'oie' in content:
+            await ctx.channel.send(f"que cê quer @{ctx.author.name}")
+            return
+        
+        #good byes
+        if 'tchau' in content or 'xau' in content or 'flw' in content or 'boa noite' in content:
+            await ctx.channel.send(f"ja vai embora porque arrombido?")
+            return
+        
+        # Usa pra ativar quando alguem fala
+        # if ctx.author.name == 'forthejacs':
+        #     await ctx.channel.send(f"que voz linda a sua")
+
+        
+
+        
+        
+    #content not aimed to him    
     if 'minha gostosa' in ctx.content.lower():
-        await ctx.channel.send(f"vai se fude luquinha!")
+        await ctx.channel.send(f"vai se fude luquinha, fica chamando os otro de minha gostosa eu ein!")
+        return
 
-    # if 'oi' in ctx.content.lower():
-    #     await ctx.channel.send(f"oi é o caralho, @{ctx.author.name}!")
-    
-    if 'foi o bototario' in ctx.content.lower():
-        await ctx.channel.send(f"foi nada vai se fude")
+    if 'foi o bot' in ctx.content.lower():
+        await ctx.channel.send(f"foi nada, se fude")
+        return
 
-    
+    if 'jac é um bot?' in ctx.content.lower():
+        await ctx.channel.send(f"ela eh!! e ta no mesmo servidor que eu")
+        return
 
 
-@bot.command(name='oi')
-async def test(ctx):
-    await ctx.send('oi é o caralho')
-
-@bot.command(name="tchau")
-async def test(ctx):
-    await ctx.send('ja vai embora porque arrombido?')
+# @bot.command(name="tchau")
+# async def tchau(ctx):
+#     await ctx.send('ja vai embora porque arrombido?')
 
 if __name__ == "__main__":
     bot.run()
